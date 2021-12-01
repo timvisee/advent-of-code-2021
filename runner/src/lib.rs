2d67e9bcf11f518d7c8ea5824dcde133314481ed
timvisee <tim@visee.me>
2021-12-01 11:51:24 +0100

    Add day01 to runner

--- runner/src/lib.rs
+++ runner/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn jobs() -> &'static [(fn(), &'static str)] {
     &[
-        // (day01a::main, "day01a"),
-        // (day01b::main, "day01b"),
+        (day01a::main, "day01a"),
+        (day01b::main, "day01b"),
         // (day02a::main, "day02a"),
         // (day02b::main, "day02b"),
         // (day03a::main, "day03a"),
