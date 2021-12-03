3113d640ef39af1bc76df34362eb17e9d203771b
timvisee <tim@visee.me>
2021-12-03 14:03:16 +0100

    Add day03a

---
+++ day03a/src/main.rs
@@ -0,0 +1,21 @@
+const WIDTH: usize = 12;
+const COUNT: usize = 1000;
+
+pub fn main() {
+    let gamma = include_str!("../input.txt")
+        .lines()
+        .map(|l| usize::from_str_radix(l, 2).unwrap())
+        .fold(vec![0; WIDTH], |count, bits| {
+            count
+                .into_iter()
+                .enumerate()
+                .map(|(i, n)| n + ((bits & 1 << i) >> i))
+                .collect()
+        })
+        .into_iter()
+        .enumerate()
+        .map(|(i, b)| ((b >= COUNT / 2) as u32) << i)
+        .sum::<u32>();
+
+    println!("{}", gamma * (!gamma & ((1 << WIDTH) - 1)));
+}
