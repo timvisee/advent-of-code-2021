7be0cdf1e350432122b58072ef43220b493ac286
timvisee <tim@visee.me>
2021-12-04 19:40:02 +0100

    Optimize day04

--- day04a/src/main.rs
+++ day04a/src/main.rs
@@ -1,26 +1,42 @@
+use std::collections::BTreeMap;
+
+const ROW: u32 = 0b11111;
+const COL: u32 = 0b100001000010000100001;
+
 pub fn main() {
     let (nums, boards) = include_str!("../input.txt").split_once("\n\n").unwrap();
 
-    let mut boards: Vec<Vec<Option<u8>>> = boards
+    let mut boards: Vec<(BTreeMap<u8, usize>, u32)> = boards
         .split("\n\n")
-        .map(|b| b.split_whitespace().map(|n| n.parse().ok()).collect())
+        .map(|b| {
+            (
+                b.split_whitespace()
+                    .enumerate()
+                    .map(|(i, n)| (n.parse().unwrap(), i))
+                    .collect(),
+                0,
+            )
+        })
         .collect();
 
-    let (board, num) = nums
+    let (board, mark, num) = nums
         .split(',')
         .map(|n| n.parse().unwrap())
-        .find_map(|num| {
-            boards.iter_mut().find_map(|board| {
-                board.iter_mut().for_each(|c| *c = c.filter(|v| *v != num));
-                ((0..5).any(|r| board[r * 5..][..5].iter().all(Option::is_none))
-                    || (0..5).any(|c| board.iter().skip(c).step_by(5).all(Option::is_none)))
-                .then(|| (board.clone(), num))
+        .find_map(|n| {
+            boards.iter_mut().find_map(|(b, m)| {
+                b.get(&n)
+                    .map(|i| *m |= 1 << *i)
+                    .filter(|_| (0..5).any(|i| *m >> i & COL == COL || *m >> (i * 5) & ROW == ROW))
+                    .map(|_| (b.clone(), *m, n))
             })
         })
         .unwrap();
 
     println!(
         "{}",
-        board.iter().map(|n| n.unwrap_or(0) as u32).sum::<u32>() * num as u32,
+        board
+            .into_iter()
+            .map(|(n, i)| (mark >> i & 1 ^ 1) * n as u32 * num as u32)
+            .sum::<u32>()
     );
 }
--- day04b/src/main.rs
+++ day04b/src/main.rs
@@ -1,24 +1,38 @@
 #![feature(drain_filter)]
 
+use std::collections::BTreeMap;
+
+const ROW: u32 = 0b11111;
+const COL: u32 = 0b100001000010000100001;
+
 pub fn main() {
     let (nums, boards) = include_str!("../input.txt").split_once("\n\n").unwrap();
 
-    let mut boards: Vec<Vec<Option<u8>>> = boards
+    let mut boards: Vec<(BTreeMap<u8, usize>, u32)> = boards
         .split("\n\n")
-        .map(|b| b.split_whitespace().map(|n| n.parse().ok()).collect())
+        .map(|b| {
+            (
+                b.split_whitespace()
+                    .enumerate()
+                    .map(|(i, n)| (n.parse().unwrap(), i))
+                    .collect(),
+                0,
+            )
+        })
         .collect();
 
-    let (board, num) = nums
+    let (board, mark, num) = nums
         .split(',')
         .map(|n| n.parse().unwrap())
         .filter_map(|n| {
             boards
-                .drain_filter(|board| {
-                    board.iter_mut().for_each(|c| *c = c.filter(|v| *v != n));
-                    (0..5).any(|r| board[r * 5..][..5].iter().all(Option::is_none))
-                        || (0..5).any(|c| board.iter().skip(c).step_by(5).all(Option::is_none))
+                .drain_filter(|(b, m)| {
+                    b.get(&n)
+                        .map(|i| *m |= 1 << *i)
+                        .map(|_| (0..5).any(|i| *m >> i & COL == COL || *m >> (i * 5) & ROW == ROW))
+                        .unwrap_or(false)
                 })
-                .map(|b| (b, n))
+                .map(|(b, m)| (b, m, n))
                 .next()
         })
         .last()
@@ -26,6 +40,9 @@ pub fn main() {
 
     println!(
         "{}",
-        board.iter().map(|n| n.unwrap_or(0) as u32).sum::<u32>() * num as u32,
+        board
+            .into_iter()
+            .map(|(n, i)| (mark >> i & 1 ^ 1) * n as u32 * num as u32)
+            .sum::<u32>()
     );
 }
