de0ec8bebc12db0f96023f7b7545749fb2270420
timvisee <tim@visee.me>
2021-12-07 17:05:21 +0100

    Revert "Optimize day07b, rounding mean is good enough"
    This reverts commit 0c397f8f2cd4ebdcbc1ed6e204aa1b6caff845eb.

--- day07b/src/main.rs
+++ day07b/src/main.rs
@@ -1,17 +1,22 @@
 pub fn main() {
     let subs = include_str!("../input.txt")
         .split(',')
-        .map(|n| n.parse::<i32>().unwrap())
+        .map(|n| n.parse().unwrap())
         .collect::<Vec<_>>();
 
-    let mean = (subs.iter().sum::<i32>() as f32 / subs.len() as f32).round() as i32;
     println!(
         "{}",
-        subs.iter()
-            .map(|n| {
-                let d = (n - mean).abs();
-                d * (d + 1) / 2
+        (subs.iter().sum::<i32>() / subs.len() as i32..)
+            .take(2)
+            .map(|t| {
+                subs.iter()
+                    .map(|n| {
+                        let d = (n - t).abs();
+                        d * (d + 1) / 2
+                    })
+                    .sum::<i32>()
             })
-            .sum::<i32>()
+            .min()
+            .unwrap()
     );
 }
