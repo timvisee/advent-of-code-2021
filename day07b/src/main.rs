e4b7a1ae30c8a63367224c04a5ad406f5ec7c3e8
timvisee <tim@visee.me>
2021-12-07 13:12:48 +0100

    Optimize day07b, search around mean

--- day07b/src/main.rs
+++ day07b/src/main.rs
@@ -1,17 +1,18 @@
 pub fn main() {
     let subs = include_str!("../input.txt")
         .split(',')
-        .map(|n| n.parse::<i32>().unwrap())
+        .map(|n| n.parse().unwrap())
         .collect::<Vec<_>>();
 
     println!(
         "{}",
-        (0..*subs.iter().max().unwrap())
+        (subs.iter().sum::<i32>() / subs.len() as i32..)
+            .take(2)
             .map(|t| {
                 subs.iter()
                     .map(|n| {
                         let d = (n - t).abs();
-                        (d * (d + 1)) / 2
+                        d * (d + 1) / 2
                     })
                     .sum::<i32>()
             })
