f3b7088e3799d9eca9e2b8935854e1e26dcd95d0
timvisee <tim@visee.me>
2021-12-07 21:03:38 +0100

    Optimize day05

--- day05a/src/main.rs
+++ day05a/src/main.rs
@@ -2,23 +2,28 @@ use atoi::atoi;
 use nom::*;
 
 pub fn main() {
-    let map = include_bytes!("../input.txt")
+    let (mut map, mut overlaps) = (vec![0u8; 1000 * 1000], 0);
+    include_bytes!("../input.txt")
         .split(|b| *b == b'\n')
         .map(|entry| {
             let ((x, y), (xx, yy)) = line(entry).unwrap().1;
             (x.min(xx), y.min(yy), x.max(xx), y.max(yy))
         })
-        .filter(|(x, y, xx, yy)| x == xx || y == yy)
-        .fold(vec![0u8; 1000 * 1000], |mut map, (x, y, xx, yy)| {
+        .for_each(|(x, y, xx, yy)| {
+            let mut mark = |x, y| {
+                if map[(x + y * 1000) as usize] == 1 {
+                    overlaps += 1;
+                }
+                map[(x + y * 1000) as usize] += 1;
+            };
             if x == xx {
-                (y..=yy).for_each(|y| map[x + y * 1000] += 1);
-            } else {
-                (x..=xx).for_each(|x| map[x + y * 1000] += 1);
+                (y..=yy).for_each(|y| mark(x, y));
+            } else if y == yy {
+                (x..=xx).for_each(|x| mark(x, y));
             }
-            map
         });
 
-    println!("{}", map.into_iter().filter(|c| *c >= 2).count());
+    println!("{}", overlaps);
 }
 
 named!(usize<&[u8], usize>, map_opt!(nom::character::complete::digit1, atoi));
--- day05b/src/main.rs
+++ day05b/src/main.rs
@@ -1,24 +1,29 @@
+#![feature(int_abs_diff)]
+
 use atoi::atoi;
 use nom::*;
 use std::iter;
 
 pub fn main() {
-    let map = include_bytes!("../input.txt").split(|b| *b == b'\n').fold(
-        vec![0u8; 1000 * 1000],
-        |mut map, entry| {
+    let (mut map, mut overlaps) = (vec![0u8; 1000 * 1000], 0);
+    include_bytes!("../input.txt")
+        .split(|b| *b == b'\n')
+        .for_each(|entry| {
             let ((x, y), (xx, yy)) = line(entry).unwrap().1;
             let range =
                 |a: isize, b: isize| iter::successors(Some(a), move |n| Some(n + (b - a).signum()));
             range(x, xx)
-                .cycle()
-                .zip(range(y, yy).cycle())
-                .take((x - xx).abs().max((y - yy).abs()) as usize + 1)
-                .for_each(|(x, y)| map[(x + y * 1000) as usize] += 1);
-            map
-        },
-    );
+                .zip(range(y, yy))
+                .take(x.abs_diff(xx).max(y.abs_diff(yy)) + 1)
+                .for_each(|(x, y)| {
+                    if map[(x + y * 1000) as usize] == 1 {
+                        overlaps += 1;
+                    }
+                    map[(x + y * 1000) as usize] += 1;
+                });
+        });
 
-    println!("{}", map.into_iter().filter(|c| *c >= 2).count());
+    println!("{}", overlaps);
 }
 
 named!(isize<&[u8], isize>, map_opt!(nom::character::complete::digit1, atoi));
