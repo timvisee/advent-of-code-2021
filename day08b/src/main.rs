c5b493d63ed13b6391e07a5e47fa65f33df97d0a
timvisee <tim@visee.me>
2021-12-08 15:45:08 +0100

    Optimize day08b, use another approach
    Don't determine position of each segment. Eliminate numbers by checking
    intersecting segments of known digits 1 and 4.

--- day08b/src/main.rs
+++ day08b/src/main.rs
@@ -1,12 +1,3 @@
-// seg:
-//    0000
-//   1    2
-//   1    2
-//    3333
-//   -    4
-//   -    4
-//    ----
-
 pub fn main() {
     println!(
         "{}",
@@ -14,21 +5,9 @@ pub fn main() {
             .lines()
             .map(|line| {
                 let (ex, digits) = line.split_once('|').unwrap();
-                let mut ex = ex.split_ascii_whitespace().collect::<Vec<_>>();
-                ex.sort_unstable_by_key(|d| d.len());
-
-                let mut seg = [0u8; 5];
-                let seg_count = ex.iter().fold([0u8; 7], |mut map, digit| {
-                    digit.bytes().for_each(|b| map[(b - b'a') as usize] += 1);
-                    map
-                });
-
-                seg[0] = ex[1].bytes().find(|&b| !ex[0].contains(b as char)).unwrap();
-                seg[1] = seg_count.iter().position(|&c| c == 6).unwrap() as u8 + b'a';
-                seg[4] = seg_count.iter().position(|&c| c == 9).unwrap() as u8 + b'a';
-                seg[2] = ex[1].bytes().find(|b| !seg.contains(b)).unwrap();
-                seg[3] = ex[2].bytes().find(|c| !seg.contains(c)).unwrap();
-
+                let ex = ex.split_ascii_whitespace().collect::<Vec<_>>();
+                let one = ex.iter().find(|d| d.len() == 2).unwrap();
+                let four = ex.iter().find(|d| d.len() == 4).unwrap();
                 digits
                     .split_ascii_whitespace()
                     .map(|d| match d.len() {
@@ -36,12 +15,19 @@ pub fn main() {
                         3 => 7,
                         4 => 4,
                         7 => 8,
-                        6 if !d.contains(seg[3] as char) => 0,
-                        6 if !d.contains(seg[2] as char) => 6,
-                        6 => 9,
-                        _ if !d.contains(seg[2] as char) => 5,
-                        _ if d.contains(seg[4] as char) => 3,
-                        _ => 2,
+                        len => match (
+                            len,
+                            d.bytes().filter(|&b| one.contains(b as char)).count(),
+                            d.bytes().filter(|&b| four.contains(b as char)).count(),
+                        ) {
+                            (5, 1, 3) => 5,
+                            (5, 2, 3) => 3,
+                            (5, _, 2) => 2,
+                            (6, 1, _) => 6,
+                            (6, _, 3) => 0,
+                            (6, _, 4) => 9,
+                            _ => unreachable!(),
+                        },
                     })
                     .enumerate()
                     .fold(0, |sum, (i, n)| sum + n * 10_u32.pow(3 - i as u32))
