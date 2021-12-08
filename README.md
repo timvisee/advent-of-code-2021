16d3a61bbe009ab51b2ad14dff2741e0f8bfff37
timvisee <tim@visee.me>
2021-12-08 14:28:08 +0100

    Optimize day08b, string slices are faster, remove obsolete segments

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ scientifically) in [`bench.rs`](./runner/src/bin/bench.rs) on an
 | [day 5](https://adventofcode.com/2021/day/5)   | [`  0.110ms`](./day05a/src/main.rs) | [`  0.220ms`](./day05b/src/main.rs) |
 | [day 6](https://adventofcode.com/2021/day/6)   | [` 0.0027ms`](./day06a/src/main.rs) | [` 0.0028ms`](./day06b/src/main.rs) |
 | [day 7](https://adventofcode.com/2021/day/7)   | [`  0.013ms`](./day07a/src/main.rs) | [`  0.013ms`](./day07b/src/main.rs) |
-| [day 8](https://adventofcode.com/2021/day/8)   | [`  0.008ms`](./day08a/src/main.rs) | [`  0.107ms`](./day08b/src/main.rs) |
+| [day 8](https://adventofcode.com/2021/day/8)   | [`  0.008ms`](./day08a/src/main.rs) | [`  0.067ms`](./day08b/src/main.rs) |
 
 ## Run solutions
 
--- day08b/src/main.rs
+++ day08b/src/main.rs
@@ -3,9 +3,9 @@
 //   1    2
 //   1    2
 //    3333
-//   4    5
-//   4    5
-//    6666
+//   -    5
+//   -    5
+//    ----
 
 pub fn main() {
     println!(
@@ -13,38 +13,34 @@ pub fn main() {
         include_str!("../input.txt")
             .lines()
             .map(|line| {
-                let (hints, digits) = line.split_once(" | ").unwrap();
-                let mut hints = hints
-                    .split_ascii_whitespace()
-                    .map(|d| d.bytes().map(|b| b - b'a').collect())
-                    .collect::<Vec<Vec<u8>>>();
-                hints.select_nth_unstable_by_key(1, Vec::len);
+                let (ex, digits) = line.split_once('|').unwrap();
+                let mut ex = ex.split_ascii_whitespace().collect::<Vec<_>>();
+                ex.sort_unstable_by_key(|d| d.len());
 
-                let mut seg = [255u8; 7];
-                let seg_count = hints.iter().fold([0u8; 7], |mut map, digit| {
-                    digit.iter().for_each(|&b| map[b as usize] += 1);
+                let mut seg = [0u8; 5];
+                let seg_count = ex.iter().fold([0u8; 7], |mut map, digit| {
+                    digit.bytes().for_each(|b| map[(b - b'a') as usize] += 1);
                     map
                 });
 
-                seg[0] = *hints[1].iter().find(|b| !hints[0].contains(b)).unwrap();
-                seg[1] = seg_count.iter().position(|&c| c == 6).unwrap() as u8;
-                seg[5] = seg_count.iter().position(|&c| c == 9).unwrap() as u8;
-                seg[2] = *hints[1].iter().find(|b| !seg.contains(b)).unwrap();
-                seg[3] = *hints[2].iter().find(|c| !seg.contains(c)).unwrap();
+                seg[0] = ex[1].bytes().find(|&b| !ex[0].contains(b as char)).unwrap();
+                seg[1] = seg_count.iter().position(|&c| c == 6).unwrap() as u8 + b'a';
+                seg[4] = seg_count.iter().position(|&c| c == 9).unwrap() as u8 + b'a';
+                seg[2] = ex[1].bytes().find(|b| !seg.contains(b)).unwrap();
+                seg[3] = ex[2].bytes().find(|c| !seg.contains(c)).unwrap();
 
                 digits
                     .split_ascii_whitespace()
-                    .map(|d| d.bytes().map(|b| b - b'a').collect::<Vec<_>>())
                     .map(|d| match d.len() {
                         2 => 1,
                         3 => 7,
                         4 => 4,
                         7 => 8,
-                        6 if !d.contains(&seg[3]) => 0,
-                        6 if !d.contains(&seg[2]) => 6,
+                        6 if !d.contains(seg[3] as char) => 0,
+                        6 if !d.contains(seg[2] as char) => 6,
                         6 => 9,
-                        _ if !d.contains(&seg[2]) => 5,
-                        _ if d.contains(&seg[5]) => 3,
+                        _ if !d.contains(seg[2] as char) => 5,
+                        _ if d.contains(seg[4] as char) => 3,
                         _ => 2,
                     })
                     .enumerate()
