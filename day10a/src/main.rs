e0784689c1821dfce1c9511fe9dff8f5228dcd33
timvisee <tim@visee.me>
2021-12-10 13:00:06 +0100

    Remove match in day10a, ASCII chars are multiples of ~30

--- day10a/src/main.rs
+++ day10a/src/main.rs
@@ -13,13 +13,7 @@ pub fn main() {
                     c => (s.pop().unwrap() != c - 2).then(|| c),
                 }))
                 .skip_while(Option::is_none)
-                .map(|c| match c.unwrap() {
-                    b')' => 3,
-                    b']' => 57,
-                    b'}' => 1197,
-                    b'>' => 25137,
-                    _ => unreachable!(),
-                })
+                .map(|c| [3, 25137, 57, 1197][c.unwrap() as usize / 30 - 1])
                 .next())
             .sum::<usize>()
     );
