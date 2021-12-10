f3dc8b83646f837a2f85a48c8e407c339f0958ba
timvisee <tim@visee.me>
2021-12-10 11:22:51 +0100

    Some day09a tweaks

--- day09a/src/main.rs
+++ day09a/src/main.rs
@@ -1,16 +1,15 @@
 pub fn main() {
-    let input = include_bytes!("../input.txt")
+    let map = include_bytes!("../input.txt")
         .split(|&b| b == b'\n')
         .collect::<Vec<_>>();
     let neighbors = [(0, -1), (0, 1), (-1, 0), (1, 0)];
 
     let mut sum = 0;
-    for (y, line) in input.iter().enumerate() {
+    for (y, line) in map.iter().enumerate() {
         for (x, cell) in line.iter().enumerate() {
             if neighbors.iter().all(|&(xx, yy)| {
-                input
-                    .get(y.overflowing_add(yy as usize).0)
-                    .and_then(|l| l.get(x.overflowing_add(xx as usize).0))
+                map.get((y as isize + yy) as usize)
+                    .and_then(|l| l.get((x as isize + xx) as usize))
                     .map(|n| cell < n)
                     .unwrap_or(true)
             }) {
