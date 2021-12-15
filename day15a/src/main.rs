c682bde00f03b75efa99984adecefe348dcaa884
timvisee <tim@visee.me>
2021-12-15 13:59:45 +0100

    Add day15a

---
+++ day15a/src/main.rs
@@ -0,0 +1,34 @@
+#![feature(int_abs_diff)]
+
+use pathfinding::directed::astar;
+
+const NEXT: [(i32, i32); 4] = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+pub fn main() {
+    let map: Vec<Vec<_>> = include_str!("../input.txt")
+        .lines()
+        .map(|l| l.bytes().map(|c| c - b'0').collect())
+        .collect();
+    let goal = (map[0].len() as i32 - 1, map.len() as i32 - 1);
+
+    println!(
+        "{}",
+        astar::astar(
+            &(0, 0),
+            |(x, y)| -> Vec<((_, _), _)> {
+                NEXT.iter()
+                    .map(|(xx, yy)| {
+                        map.get((y + yy) as usize)
+                            .and_then(|r| r.get((x + xx) as usize))
+                            .map(|c| ((x + xx, y + yy), *c as u32))
+                    })
+                    .flatten()
+                    .collect()
+            },
+            |p| (p.0.abs_diff(goal.0) + p.1.abs_diff(goal.1)) as u32 / 3,
+            |&p| p == goal,
+        )
+        .unwrap()
+        .1
+    );
+}
