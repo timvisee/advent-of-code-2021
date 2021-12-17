45eed696201f30f40f9340d6b3cf62a11e74472a
timvisee <tim@visee.me>
2021-12-17 16:56:34 +0100

    Optimize day16, slice slices

--- day16a/src/main.rs
+++ day16a/src/main.rs
@@ -1,35 +1,54 @@
-use bytes::BytesMut;
-
 pub fn main() {
-    let mut bits = include_str!("../input.txt")
+    let bits = include_str!("../input.txt")
         .trim()
         .chars()
         .flat_map(|c| {
             let n = c.to_digit(16).unwrap();
             (0..4).map(move |i| (1 & n >> (3 - i)) as u8)
         })
-        .collect();
-    println!("{}", packet(&mut bits));
+        .collect::<Vec<u8>>();
+    println!("{}", packet(&bits).0);
 }
 
-fn packet(bits: &mut BytesMut) -> u16 {
-    let mut ver = num(&bits.split_to(3));
-    if num(&bits.split_to(3)) == 4 {
-        (0..).take_while(|_| bits.split_to(5)[0] == 1).count();
-    } else {
-        if bits.split_to(1)[0] == 0 {
-            let len = num(&bits.split_to(15)) as usize;
-            let mut bits = bits.split_to(len);
-            while !bits.is_empty() {
-                ver += &packet(&mut bits);
-            }
-        } else {
-            ver += (0..num(&bits.split_to(11)))
-                .map(|_| packet(bits))
-                .sum::<u16>();
-        }
+fn packet(mut bits: &[u8]) -> (u16, &[u8]) {
+    let mut ver = num(&bits[0..3]);
+    let typ = num(&bits[3..6]);
+    bits = &bits[6..];
+    if typ == 4 {
+        (0..)
+            .take_while(|_| {
+                let cont = bits[0] == 1;
+                bits = &bits[5..];
+                cont
+            })
+            .count();
+        return (ver, bits);
     }
-    ver
+
+    let split = bits.split_at(1);
+    bits = split.1;
+    if split.0[0] == 0 {
+        let len = num(&bits[0..15]) as usize;
+        bits = &bits[15..];
+        let (mut payload, extra) = bits.split_at(len);
+        bits = extra;
+        while !payload.is_empty() {
+            let packet = packet(payload);
+            payload = packet.1;
+            ver += packet.0;
+        }
+    } else {
+        let split = bits.split_at(11);
+        bits = split.1;
+        ver += (0..num(split.0))
+            .map(|_| {
+                let packet = packet(bits);
+                bits = packet.1;
+                packet.0
+            })
+            .sum::<u16>();
+    }
+    (ver, bits)
 }
 
 fn num(b: &[u8]) -> u16 {
--- day16b/src/main.rs
+++ day16b/src/main.rs
@@ -1,50 +1,62 @@
-use bytes::BytesMut;
-
 pub fn main() {
-    let mut bits = include_str!("../input.txt")
+    let bits = include_str!("../input.txt")
         .trim()
         .chars()
         .flat_map(|c| {
             let n = c.to_digit(16).unwrap();
             (0..4).map(move |i| (1 & n >> (3 - i)) as u8)
         })
-        .collect();
-    println!("{}", packet(&mut bits)[0]);
+        .collect::<Vec<u8>>();
+    println!("{}", packet(&bits).0[0]);
 }
 
-fn packet(bits: &mut BytesMut) -> Vec<usize> {
-    let typ = num(&bits.split_to(6)[3..]);
+fn packet(mut bits: &[u8]) -> (Vec<usize>, &[u8]) {
+    let typ = num(&bits[3..6]);
+    bits = &bits[6..];
     if typ == 4 {
         let mut num_bits = vec![];
         (0..)
             .take_while(|_| {
-                let bits = bits.split_to(5);
-                num_bits.extend(&bits[1..]);
-                bits[0] == 1
+                let cont = bits[0] == 1;
+                num_bits.extend(&bits[1..5]);
+                bits = &bits[5..];
+                cont
             })
             .count();
-        return vec![num(&num_bits)];
+        return (vec![num(&num_bits)], bits);
     }
 
     let mut nums = vec![];
-    if bits.split_to(1)[0] == 0 {
-        let len = num(&bits.split_to(15)) as usize;
-        let mut bits = bits.split_to(len);
-        while !bits.is_empty() {
-            nums.extend_from_slice(&packet(&mut bits));
+    let split = bits.split_at(1);
+    bits = split.1;
+    if split.0[0] == 0 {
+        let len = num(&bits[0..15]) as usize;
+        bits = &bits[15..];
+        let (mut payload, extra) = bits.split_at(len);
+        bits = extra;
+        while !payload.is_empty() {
+            let packet = packet(payload);
+            payload = packet.1;
+            nums.extend_from_slice(&packet.0);
         }
     } else {
-        (0..num(&bits.split_to(11))).for_each(|_| nums.extend_from_slice(&packet(bits)));
+        let split = bits.split_at(11);
+        bits = split.1;
+        (0..num(split.0)).for_each(|_| {
+            let packet = packet(bits);
+            bits = packet.1;
+            nums.extend_from_slice(&packet.0);
+        });
     }
 
     match typ {
-        0 => vec![nums.iter().sum()],
-        1 => vec![nums.iter().product()],
-        2 => vec![*nums.iter().min().unwrap()],
-        3 => vec![*nums.iter().max().unwrap()],
-        5 => vec![(nums[0] > nums[1]) as usize],
-        6 => vec![(nums[0] < nums[1]) as usize],
-        7 => vec![(nums[0] == nums[1]) as usize],
+        0 => (vec![nums.iter().sum()], bits),
+        1 => (vec![nums.iter().product()], bits),
+        2 => (vec![*nums.iter().min().unwrap()], bits),
+        3 => (vec![*nums.iter().max().unwrap()], bits),
+        5 => (vec![(nums[0] > nums[1]) as usize], bits),
+        6 => (vec![(nums[0] < nums[1]) as usize], bits),
+        7 => (vec![(nums[0] == nums[1]) as usize], bits),
         _ => unreachable!(),
     }
 }
