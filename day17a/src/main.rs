4eda1238ffb1d4837e9363c387d6d3d162cbc04c
timvisee <tim@visee.me>
2021-12-20 12:47:59 +0100

    Optimize day17a

--- day17a/src/main.rs
+++ day17a/src/main.rs
@@ -5,7 +5,7 @@ pub fn main() {
         .split_once(", y=")
         .unwrap();
     let (x, y) = (x.split_once("..").unwrap(), y.split_once("..").unwrap());
-    let target: (isize, isize, isize, isize) = (
+    let target: (_, i32, _, _) = (
         x.0.parse().unwrap(),
         y.0.parse().unwrap(),
         x.1.parse().unwrap(),
@@ -21,21 +21,33 @@ pub fn main() {
     );
 }
 
-fn fire(target: (isize, isize, isize, isize), v: (isize, isize)) -> Option<isize> {
-    let mut high = 0;
-    for (x, y, _, _) in path(v) {
-        if x > target.2 || y < target.1 {
+fn fire(target: (i32, i32, i32, i32), mut v: (i32, i32)) -> Option<i32> {
+    let vy = v.1;
+    let mut p = (0, 0);
+    if v.1 > 1 {
+        for _ in 0..v.1 * 2 + 1 {
+            p.0 += v.0;
+            v.0 -= 1;
+            if p.0 > target.2 {
+                return None;
+            } else if v.0 == 0 {
+                break;
+            }
+        }
+        v.1 = -v.1 - 1;
+    }
+    for (x, y, vx, _) in path(p, v) {
+        if vx == 0 && x < target.0 || x > target.2 || y < target.1 {
             return None;
         } else if x >= target.0 && x <= target.2 && y >= target.1 && y <= target.3 {
-            break;
+            return Some(vy * (vy + 1) / 2);
         }
-        high = high.max(y);
     }
-    Some(high)
+    unreachable!();
 }
 
-fn path(v: (isize, isize)) -> impl Iterator<Item = (isize, isize, isize, isize)> {
-    std::iter::successors(Some((0, 0, v.0, v.1)), |p| {
+fn path(p: (i32, i32), v: (i32, i32)) -> impl Iterator<Item = (i32, i32, i32, i32)> {
+    std::iter::successors(Some((p.0, p.1, v.0, v.1)), |p| {
         Some((p.0 + p.2, p.1 + p.3, (p.2 - 1).max(0), p.3 - 1))
     })
 }
