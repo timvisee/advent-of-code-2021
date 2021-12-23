8abf9dbbe57bab3e797276bcb3cc9326ee2d1a24
timvisee <tim@visee.me>
2021-12-23 17:16:03 +0100

    Tweak day17a

--- day17a/src/main.rs
+++ day17a/src/main.rs
@@ -1,12 +1,14 @@
 pub fn main() {
-    let y = include_str!("../input.txt")
+    let n = -include_str!("../input.txt")
         .trim()
         .trim_start_matches("target area: x=")
         .split_once(", y=")
         .unwrap()
         .1
-        .split_once("..")
-        .unwrap();
-    let n = -y.0.parse::<i32>().unwrap().min(y.1.parse::<i32>().unwrap()) - 1;
+        .splitn(2, "..")
+        .map(|n| n.parse::<i32>().unwrap())
+        .min()
+        .unwrap()
+        - 1;
     println!("{}", (n * (n + 1)) / 2);
 }
